bf5d60a5a4d8516b4a00b188285b4255d3c04bce
164638896@qq.com <Lai198626>
2019-05-21 17:10:36 +0800

    改为线程池 下载。

--- Assets/Scripts/HttpDownLoad.cs
+++ Assets/Scripts/HttpDownLoad.cs
@@ -6,6 +6,15 @@ using ICSharpCode.SharpZipLib.Zip;
 
 public class HttpDownLoad
 {
+    public class ThreadParam
+    {
+        public int m_nID = 0;
+        public string m_strURL = "";
+        public string m_strSavePath = "";
+        public Action<ThreadParam> callBack;
+    }
+
+
     public float progress { get; private set; }
     //涉及子线程要注意,Unity关闭的时候子线程不会关闭，所以要有一个标识
     private bool isStop;
@@ -18,104 +27,99 @@ public class HttpDownLoad
     /// <param name="url">URL下载地址</param>
     /// <param name="savePath">Save path保存路径</param>
     /// <param name="callBack">Call back回调函数</param>
-    public void DownLoad(string url, string savePath, Action callBack)
+    public void DownLoad(object param)
     {
+        ThreadParam tp = param as ThreadParam;
+
         isStop = false;
-        thread = new Thread(delegate ()
+        if (!Directory.Exists(tp.m_strSavePath))
         {
-            if (!Directory.Exists(savePath))
-            {
-                Directory.CreateDirectory(savePath);
-            }
+            Directory.CreateDirectory(tp.m_strSavePath);
+        }
 
-            //获取下载文件的总长度
-            long totalLength = GetLength(url);
-            if (totalLength > 0)
+        //获取下载文件的总长度
+        long totalLength = GetLength(tp.m_strURL);
+        if (totalLength > 0)
+        {
+            try
             {
-                try
+                //这是要下载的文件路径
+                string filePath = tp.m_strSavePath + "/" + tp.m_strURL.Substring(tp.m_strURL.LastIndexOf('/') + 1);
+
+                //使用流操作文件
+                FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+                long fileLength = fs.Length;
+
+                //如果没下载完
+                if (fileLength < totalLength)
                 {
-                    //这是要下载的文件路径
-                    string filePath = savePath + "/" + url.Substring(url.LastIndexOf('/') + 1);
+                    //断点续传核心，设置本地文件流的起始位置
+                    fs.Seek(fileLength, SeekOrigin.Begin);
 
-                    //使用流操作文件
-                    FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
-                    long fileLength = fs.Length;
+                    HttpWebRequest request = HttpWebRequest.Create(tp.m_strURL) as HttpWebRequest;
 
-                    //如果没下载完
-                    if (fileLength < totalLength)
+                    request.Timeout = 5000;
+                    //断点续传核心，设置远程访问文件流的起始位置
+                    request.AddRange((int)fileLength);
+                    Stream stream = request.GetResponse().GetResponseStream();
+
+                    byte[] buffer = new byte[40960];
+                    //使用流读取内容到buffer中
+                    //注意方法返回值代表读取的实际长度,并不是buffer有多大，stream就会读进去多少
+                    int length = stream.Read(buffer, 0, buffer.Length);
+                    while (length > 0)
                     {
-                        //断点续传核心，设置本地文件流的起始位置
-                        fs.Seek(fileLength, SeekOrigin.Begin);
+                        //UnityEngine.Debug.Log("length:" + length);
 
-                        HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
-
-                        request.Timeout = 5000;
-                        //断点续传核心，设置远程访问文件流的起始位置
-                        request.AddRange((int)fileLength);
-                        Stream stream = request.GetResponse().GetResponseStream();
-
-                        byte[] buffer = new byte[40960];
-                        //使用流读取内容到buffer中
-                        //注意方法返回值代表读取的实际长度,并不是buffer有多大，stream就会读进去多少
-                        int length = stream.Read(buffer, 0, buffer.Length);
-                        while (length > 0)
-                        {
-                            //UnityEngine.Debug.Log("length:" + length);
-
-                            //如果Unity客户端关闭，停止下载
-                            if (isStop) break;
-                            //将内容再写入本地文件中
-                            fs.Write(buffer, 0, length);
-                            //计算进度
-                            fileLength += length;
-                            progress = (float)fileLength / (float)totalLength;
-                            //UnityEngine.Debug.Log(progress);
-                            fs.Flush();
-                            //类似尾递归
-                            length = stream.Read(buffer, 0, buffer.Length);
-                        }
-
-                        stream.Close();
-                        stream.Dispose();
-                    }
-                    else
-                    {
-                        progress = fileLength / totalLength;
+                        //如果Unity客户端关闭，停止下载
+                        if (isStop) break;
+                        //将内容再写入本地文件中
+                        fs.Write(buffer, 0, length);
+                        //计算进度
+                        fileLength += length;
+                        progress = (float)fileLength / (float)totalLength;
+                        //UnityEngine.Debug.Log(progress);
+                        fs.Flush();
+                        //类似尾递归
+                        length = stream.Read(buffer, 0, buffer.Length);
                     }
 
-                    fs.Close();
-                    fs.Dispose();
-
-                    //如果下载完毕，执行回调
-                    if (progress >= 1)
-                    {
-                        isDone = true;
-                        if (callBack != null) callBack();
-
-                        if (progress > 1)
-                        {
-                            UnityEngine.Debug.Log("下载错误:" + progress * 100 + "%");
-                        }
-                    }
-
-                    UnityEngine.Debug.Log("下载进度为:" + progress * 100 + "%");
+                    stream.Close();
+                    stream.Dispose();
                 }
-                catch (WebException ex)
+                else
                 {
-                    UnityEngine.Debug.Log("WebException Error code: " + ex.Status);
-
-                    //if (callBack != null) callBack();
+                    progress = fileLength / totalLength;
                 }
+
+                fs.Close();
+                fs.Dispose();
+
+                //如果下载完毕，执行回调
+                if (progress >= 1)
+                {
+                    isDone = true;
+                    if (tp.callBack != null) tp.callBack(tp);
+
+                    if (progress > 1)
+                    {
+                        UnityEngine.Debug.Log("下载错误:" + progress * 100 + "%");
+                    }
+                }
+
+                UnityEngine.Debug.Log("下载进度为:" + progress * 100 + "%");
             }
-            else
+            catch (WebException ex)
             {
+                UnityEngine.Debug.Log("WebException Error code: " + ex.Status);
+
                 //if (callBack != null) callBack();
             }
-        });
-
-        //开启子线程
-        thread.IsBackground = true;
-        thread.Start();
+        }
+        else
+        {
+            //if (callBack != null) callBack();
+        }
     }
 
     /// <summary>
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,28 +5,49 @@ using UnityEngine.UI;
 using System;
 using System.Net;
 using System.IO;
+using System.Threading;
+using System.Collections.Generic;
 
 public class Test : MonoBehaviour {
 
-	bool isDone;
 	Slider slider;
 	Text text;
+    HttpDownLoad http;
 
-	void Awake()
+    private List<HttpDownLoad.ThreadParam> mDownLoadList = new List<HttpDownLoad.ThreadParam>();
+
+    void Awake()
 	{
 		slider = GameObject.Find("Slider").GetComponent<Slider>();
 		text = GameObject.Find("Text").GetComponent<Text>();
 	}
 
-	HttpDownLoad http;
-	string testURL = @"http://127.0.0.1/SharpZipLib_0860_Bin.zip";
-	string savePath;
-	
 	void Start ()
     {
-		savePath = Application.streamingAssetsPath;
 		http = new HttpDownLoad();
-		http.DownLoad(testURL, savePath, LoadLevel);
+
+        HttpDownLoad.ThreadParam param = new HttpDownLoad.ThreadParam();
+        param.m_nID = 1;
+        param.m_strURL = @"http://127.0.0.1/SharpZipLib_0860_Bin.zip";
+        param.m_strSavePath = Application.streamingAssetsPath;
+        param.callBack = ThreadCallBack;
+        bool ret = ThreadPool.QueueUserWorkItem(http.DownLoad, param);
+        if (!ret)
+        {
+            UnityEngine.Debug.LogError("");
+        }
+
+        HttpDownLoad.ThreadParam param1 = new HttpDownLoad.ThreadParam();
+        param1.m_nID = 2;
+        param1.m_strURL = @"http://127.0.0.1/main.6.com.top1game.rotdgp.zip";
+        param1.m_strSavePath = Application.streamingAssetsPath;
+        param1.callBack = ThreadCallBack;
+
+        bool ret1 = ThreadPool.QueueUserWorkItem(http.DownLoad, param1);
+        if (!ret1)
+        {
+            UnityEngine.Debug.LogError("");
+        }
     }
 
 	void OnDisable()
@@ -35,29 +56,35 @@ public class Test : MonoBehaviour {
 		http.Close();
 	}
 
-	void LoadLevel()
+	void ThreadCallBack(HttpDownLoad.ThreadParam param)
 	{
-		isDone = true;
-	}
+        lock (mDownLoadList)
+        {
+            mDownLoadList.Add(param);
+        }
+    }
 
 	void Update()
 	{
 		slider.value = http.progress;
-		text.text = "资源加载中" + (slider.value * 100).ToString("0.00") + "%"; 
-		if(isDone)
-		{
-            UnityEngine.Debug.Log("加载完成");
-            isDone = false;
-            HttpDownLoad.UnZipFile(savePath + "/SharpZipLib_0860_Bin.zip", savePath);
-            StartCoroutine(LoadScene(@"file://" + Application.streamingAssetsPath + "/" + testURL.Substring(testURL.LastIndexOf('/') + 1)));
-		}
+		text.text = "资源加载中" + (slider.value * 100).ToString("0.00") + "%";
+        lock (mDownLoadList)
+        {
+            foreach(HttpDownLoad.ThreadParam param in mDownLoadList)
+            {
+                string fileName = Path.GetFileName(param.m_strURL);
+                HttpDownLoad.UnZipFile(param.m_strSavePath + "/" + fileName, param.m_strSavePath);
+            }
+
+            mDownLoadList.Clear();
+        }
 	}
 
-	IEnumerator LoadScene(string url)
-	{
-		WWW www = new WWW(url);
-		yield return www;
-		//AssetBundle ab = www.assetBundle;
-		//SceneManager.LoadScene("Demo2_towers");
-	}
+	//IEnumerator LoadScene(string url)
+	//{
+	//	WWW www = new WWW(url);
+	//	yield return www;
+	//	//AssetBundle ab = www.assetBundle;
+	//	//SceneManager.LoadScene("Demo2_towers");
+	//}
 }
